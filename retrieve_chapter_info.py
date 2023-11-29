e39d09b023503ae85f27d8590d51d851060dff1e
complanar <post@complanar.de>
2023-11-29 01:36:02 +0100

    Update again to match flake8.

--- retrieve_chapter_info.py
+++ retrieve_chapter_info.py
@@ -3,14 +3,15 @@
 import xml.etree.ElementTree as ET
 
 osisFile = "Elberfelder 2003.osis.xml"
+ns = '{http://www.bibletechnologies.net/2003/OSIS/namespace}'
 chapterVerses = []
 
 tree = ET.parse(osisFile)
 root = tree.getroot()
 
-for chapter in root.iter('{http://www.bibletechnologies.net/2003/OSIS/namespace}chapter'):
+for chapter in root.iter(ns + 'chapter'):
     verses = 0
-    for verse in chapter.findall('{http://www.bibletechnologies.net/2003/OSIS/namespace}verse'):
+    for verse in chapter.findall(ns + 'verse'):
         verses += 1
     chapterVerses.append(f"{chapter.attrib['osisID']}\t{verses}")
     print(f"Found {verses} verses in chapter {chapter.attrib['osisID']}")
